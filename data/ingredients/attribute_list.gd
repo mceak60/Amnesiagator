11282b6f6545e937747247a6a0aa6db2d5a7da52
ehyatt13 <ethan.hyatt13@gmail.com>
2025-01-02 15:22:14 -0500

    added attribute list

---
+++ data/ingredients/attribute_list.gd
@@ -0,0 +1,13 @@
+class_name AttributeList
+extends Resource
+
+@export var attribute_list: Dictionary = {
+	"hot" = 0,
+	"cold" = 0,
+	"tangy" = 0,
+}
+
+
+
+func _to_string() -> String:
+	return name
--- data/ingredients/ingredient_details.gd
+++ data/ingredients/ingredient_details.gd
@@ -3,18 +3,28 @@ extends Resource
 
 enum Quality {CHEAP, AVERAGE, EXQUISITE}
 
+enum Ingredient_Type {BASE, MIXER, GARNISH}
+
+const DEFAULT_ATTRIBUTES := {
+	"Hot": 0,
+	"Cold" : 0,
+	"Tangy" : 0
+}
+
 const QUALITY_COLORS := {
 	Quality.CHEAP: Color("124a2e"),
 	Quality.AVERAGE: Color("1c527c"),
 	Quality.EXQUISITE: Color("ab0979"),
 }
 
+
 @export var name: String
 
 @export_category("Data")
 @export var quality: Quality
+@export var ingredient_type: Ingredient_Type
 @export var gold_cost := 1
-@export var trait_list: Array[String]
+@export var attribute_list: Dictionary = DEFAULT_ATTRIBUTES
 
 @export_category("Visuals")
 @export var skin_coordinates: Vector2i
