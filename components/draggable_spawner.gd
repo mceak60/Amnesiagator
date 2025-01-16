f9669b6cb92b68d8efd00e35f1d982ef74fc6768
Nick Gonzalez <mceak60@gmail.com>
2025-01-15 20:33:50 -0600

    Added Debug Labels
    ...for draggables, i.e. ingredients, customers, and drinks

--- components/draggable_spawner.gd
+++ components/draggable_spawner.gd
@@ -28,7 +28,7 @@ func _ready() -> void:
 	tween.tween_callback(spawn_ingredient_at.bind(preload("res://data/ingredients/garnishes/hickory.tres"), Vector2i(3, 4)))
 	tween.tween_callback(spawn_drink_at.bind(Vector2i(2, 0)))
 	#tween.tween_callback(spawn_customer_at.bind(preload("res://data/customers/doug.tres"), Vector2i(1, 0)))
-	tween.tween_callback(spawn_customer_at.bind(preload("res://data/customers/test.tres"), Vector2i(4, 0)))
+	#tween.tween_callback(spawn_customer_at.bind(preload("res://data/customers/test.tres"), Vector2i(4, 0)))
 	#tween.tween_callback(spawn_customer_at.bind(preload("res://data/customers/mark.tres"), Vector2i(4, 0)))
 
 
